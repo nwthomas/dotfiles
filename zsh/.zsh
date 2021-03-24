3d6dcc140e2ba41d719f64ec95aa83deb594b768
Nathan Thomas <nwthomas@me.com>
2021-03-23 22:09:26 -0700

    include base .zsh file

---
+++ zsh/.zsh
@@ -0,0 +1,9 @@
+ZSH_THEME="robbyrussell"
+
+plugins=(colored-man-pages colorize command-not-found git)
+
+source $ZSH/oh-my-zsh.sh
+
+alias c="clear"
+alias edit="code ~/.zshrc"
+alias gac="git commit -a -m"
